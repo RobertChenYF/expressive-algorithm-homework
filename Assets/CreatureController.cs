413badf994767775401d47f1c19595645c130159
Robert <chen1fan@icloud.com>
2020-02-14 05:37:42 -0500

    almost finish

--- Assets/CreatureController.cs
+++ Assets/CreatureController.cs
@@ -6,43 +6,87 @@ public class CreatureController : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public static float speed = 2;
+    public float speed = 3;
+    private float currentSpeed;
     public float energy = 1;
-    public float size;
+    public float SenseCircleSize = 1;
     public Collider2D senseCircle;
     private int foodFound = 0;
     private bool insideSafeZone;
     public Rigidbody2D rigidbodyOfCreature;
     public Collider2D creatureBody;
     //private bool targetLocked = false;
-    private float dayTimeLeft = 20;
+    
     public GameObject creature;
     private int dayExisted = 0;
     private int NightCycleTime = 0;
+    
     private Vector3 home;
     private GameObject targetFood;
     private Vector3 upFromLastFrame;
     private float timerForDirectionChange = 3f;
     private bool BackHome = false;
-    private bool NightRoutineRun;
-    private bool DayRoutineRun;
+    private bool NightRoutineRun = false;
+    private bool DayRoutineRun = true;
+    public bool justSpawned = true;
+    public GameObject detectionCycle;
+    private BoxCollider2D foodSpawnBox;
+    private SpriteRenderer spriteRenderer;
+    public int currentGeneration = 1;
     void Start()
     {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        
+        currentSpeed = speed;
         rigidbodyOfCreature = gameObject.GetComponent<Rigidbody2D>();
         home = transform.position;
-        DayTimeStart();
+        DayRoutineRun = false;
+        DayTimeStart(DayRoutineRun);
+        foodSpawnBox = GameObject.Find("FoodSpawnBox").GetComponent<BoxCollider2D>();
+        spriteRenderer.color = new Color(0.8f- (speed-3)*0.7f,1,0.8f-(SenseCircleSize-1));
+        detectionCycle.transform.localScale = new Vector3(SenseCircleSize , SenseCircleSize, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (foodFound >= 2)
+            {
+                //reproduce
+
+                Vector3 newHomeLocation = transform.position + new Vector3(Random.Range(-0.6f, 0.6f), Random.Range(-0.6f, 0.6f), 0);
+              //  while (foodSpawnBox.bounds.Contains(newHomeLocation))
+              //  {
+              //      newHomeLocation = transform.position + new Vector3(Random.Range(-0.6f, 0.6f), Random.Range(-0.6f, 0.6f), 0);
+               // }
+                GameObject NewBornCreature = Instantiate(creature, transform.position + new Vector3(Random.Range(0.5f, 0.6f), Random.Range(0.5f, 0.6f), 0), Quaternion.identity);
+                NewBornCreature.GetComponent<CreatureController>().speed = speed + Random.Range(-0.5f,0.5f);
+                NewBornCreature.GetComponent<CreatureController>().currentGeneration = currentGeneration + 1;
+                NewBornCreature.GetComponent<CreatureController>().SenseCircleSize = SenseCircleSize + Random.Range(-0.1f,0.1f);
+                NewBornCreature.name = "creature generation" + (currentGeneration+1).ToString();
+                print("reproduce");
+                //newCreature.GetComponents<CreatureController>(). = ;
+            }
+
+            GameObject newCreature = Instantiate(creature, transform.position, Quaternion.identity);
+            newCreature.GetComponent<CreatureController>().speed = speed;
+            newCreature.GetComponent<CreatureController>().currentGeneration = currentGeneration;
+            newCreature.GetComponent<CreatureController>().SenseCircleSize = SenseCircleSize;
+            newCreature.name = "creature generation" + (currentGeneration).ToString();
+            Destroy(gameObject);
+        }
+        
         if (EnvironmentController.Daytime == true)
         {
-        rigidbodyOfCreature.velocity = transform.up * speed;
-        if (targetFood == null)
-        {
-            transform.up = upFromLastFrame;
+            justSpawned = false;
+
+            if (targetFood == null && BackHome == false)
+            {//wandering
+                transform.up = upFromLastFrame;
             if (timerForDirectionChange <= 0)
             {
                 timerForDirectionChange = 1.5f;
@@ -51,57 +95,80 @@ public class CreatureController : MonoBehaviour
             }
 
             timerForDirectionChange -= Time.deltaTime;
-        }
-
-          if (foodFound == 2 || ((foodFound == 1) && EnvironmentController.dayTimeTimer < 3))
+                rigidbodyOfCreature.velocity = transform.up * currentSpeed;
+            }
+        else if (targetFood != null && BackHome == false)
+            {
+                //sense food
+                transform.up = targetFood.transform.position - transform.position; 
+                rigidbodyOfCreature.velocity = transform.up * currentSpeed;
+            }
+         if ((((foodFound >= 2) || ((foodFound == 1) && EnvironmentController.dayTimeTimer < 4))) && (Vector2.Distance(home, transform.position) > 0.1f))
         {
             //go back to home
             BackHome = true;
+                print("going home");
                 transform.up = home - transform.position;
+                rigidbodyOfCreature.velocity = transform.up * currentSpeed;
             }
+           if(BackHome && Vector2.Distance(home,transform.position) < 0.1f)
+            {
+                insideSafeZone = true;
+                print("home success");
+                currentSpeed = 0;
+            }  
+
+          
         }
        
 
-        if (EnvironmentController.Daytime == false)
+        if (EnvironmentController.Daytime == false && EnvironmentController.dayTimeTimer <=0)
         {
-            NightTime();
+            NightTime(NightRoutineRun);
         }
         
     }
     
-    void DayTimeStart()
+    void DayTimeStart(bool DayTimeStartRuned)
     {
+        if (DayTimeStartRuned == false)
+        {
         dayExisted++;
         transform.up = new Vector3(Random.Range(-3f,3f), Random.Range(-2f, 2f), 0) - transform.position;
         upFromLastFrame = transform.up;
+        DayTimeStartRuned = true;
+
+        }
+        
     }
-    void NightTime()
+    void NightTime(bool NightTimeRuned)
     {
 
         if (NightCycleTime < dayExisted)
         {
-        if (foodFound == 0 || transform.position != home)
+        if (foodFound == 0 || insideSafeZone == false)
         {
-            // die
-            Destroy(gameObject);
+                // die
+                if (justSpawned != true)
+                {
+                Destroy(gameObject);
+                Debug.Log("die");
+                }
+            
         }
-        if (foodFound >= 2)
-        {
-            //reproduce
-            GameObject newCreature = Instantiate(creature, transform.position, Quaternion.identity);
-            //newCreature.GetComponents<CreatureController>(). = ;
-        }
+        
+            NightTimeRuned = true;
+            
             NightCycleTime++;
-            foodFound = 0;
         }
-       
+        
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("food") && targetFood == null && foodFound < 2)
         {
-            transform.up = col.gameObject.transform.position - transform.position;
+            //transform.up = col.gameObject.transform.position - transform.position;
             targetFood = col.gameObject;
             Debug.Log("lock");
         }
@@ -109,14 +176,13 @@ public class CreatureController : MonoBehaviour
 
     }
 
-    
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("food"))
         {
             foodFound++;
             Destroy(collision.gameObject) ;
-            Debug.Log(foodFound);
+            Debug.Log("eat");
             //targetLocked = false;
            
         }
--- Assets/EnvironmentController.cs
+++ Assets/EnvironmentController.cs
@@ -1,35 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class EnvironmentController : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public static float landSize;
-    public static float foodAmount;
+    
     public static bool Daytime = true;
     public static float dayTimeTimer = 10;
-
-
+    public static int state;
+    public Slider slider;
+    public TextMeshProUGUI textMeshUI;
+    public TextMeshProUGUI foodAmountText;
+    public TextMeshProUGUI PressSpace;
+    public TextMeshProUGUI PopulationText;
+    public int foodAmount;
+    public GameObject food;
 
     void Start()
     {
-        
+        for (int i = 0; i < foodAmount; i++)
+        {
+            Vector3 foodLocation = new Vector3(Random.Range(-6.0f, 6.0f), Random.Range(-3.0f, 3.0f), 0);
+            Instantiate(food, foodLocation, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        textMeshUI.text = "Time left: " + ((int)dayTimeTimer).ToString();
+        foodAmountText.text = "Food Amount: " + ((int)(slider.value)).ToString();
+
+
+        int population = 0;
+        foreach (GameObject creature in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            population++;
+        }
+        PopulationText.text = "Poluation: " + population.ToString();
         if (Daytime)
         {
             dayTimeTimer -= Time.deltaTime;
+            PressSpace.enabled = false;
         }
         if (dayTimeTimer<= 0)
         {
+            
             Daytime = false;
+            PressSpace.enabled = true;
+           
         }
+        if (Input.GetKeyDown(KeyCode.Space) && dayTimeTimer <= 0)
+        {
 
+            foreach (GameObject foodFromYesterday in GameObject.FindGameObjectsWithTag("food"))
+            {
+                Destroy(foodFromYesterday);
+            }
+            foodAmount = (int)slider.value;
+          for (int i = 0; i < foodAmount; i++)
+           {
+                Vector3 foodLocation = new Vector3(Random.Range(-6.0f, 6.0f), Random.Range(-3.0f, 3.0f), 0);
+                Instantiate(food, foodLocation, Quaternion.identity);
+            }
+            Daytime = true;
+            dayTimeTimer = 10;
+            
+        }
 
     }
 }
